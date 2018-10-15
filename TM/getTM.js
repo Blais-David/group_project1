27b2896ca4828e66854fefbb9a160ffb7bc491e3
Anna Nichols <43146489+anicho91@users.noreply.github.com>
2018-10-15 11:20:36 -0400

    Update getTM.js

--- TM/getTM.js
+++ TM/getTM.js
@@ -1,19 +1,8 @@
-// https://app.ticketmaster.com/discovery/v2/events.json?apikey=MdV4MZfCLGKGQ2tYAK3tZAsVQk74dUcp&keyword=${artistName}&radius=${userRadius}&city=${userCity}
-// https://app.ticketmaster.com/discovery/v2/events.json?apikey=MdV4MZfCLGKGQ2tYAK3tZAsVQk74dUcp&keyword=${artistName}
-// https://app.ticketmaster.com/discovery/v2/events.json?apikey=MdV4MZfCLGKGQ2tYAK3tZAsVQk74dUcp&keyword=${artistName}&latlong=${lat},${long}&radius=100
-
 const getTM = function(artistName) {
-  
-  
-  // navigator.geolocation.getCurrentPosition(function(position){
-  //   const lat = (position.coords.latitude);
-  //   const long = (position.coords.longitude);
-  //   console.log(lat, long)
-  
 
   const queryURL = `https://app.ticketmaster.com/discovery/v2/events.json?apikey=MdV4MZfCLGKGQ2tYAK3tZAsVQk74dUcp&keyword=${artistName}`;
-  // const queryURL = `https://app.ticketmaster.com/discovery/v2/events.json?apikey=MdV4MZfCLGKGQ2tYAK3tZAsVQk74dUcp&keyword=${artistName}&radius=100&latlong=${lat},${long}`
-  console.log(queryURL);
+
+
   $.ajax({
     url: queryURL,
     method: "GET"
@@ -30,7 +19,6 @@ const getTM = function(artistName) {
         const newDiv = $("<div>");
 
         if (response._embedded.events[i].classifications[0].segment.name == "Music"){
-        //   console.log(response._embedded.events[i].dates.start.localDate);
         let eventsList = response._embedded.events[i].name;
         let eventDate = response._embedded.events[i].dates.start.localDate;
         let eventURL = response._embedded.events[i].url;
@@ -40,9 +28,6 @@ const getTM = function(artistName) {
         img1.alt = 'Artist Image';
         img1.height = '300'
         display0.append(img1);
-        
-        // console.log(artistImg);
-        // console.log(eventsList);
             
 
         newDiv.html(`<h1>${eventsList}</h1><p><em>${eventDate}</em></p>`);
@@ -50,8 +35,7 @@ const getTM = function(artistName) {
         display.append(newDiv);
 
         for (let j = 0; j < response._embedded.events[i]._embedded.venues.length; j++) {
-          // console.log(response._embedded.events[i]._embedded.venues)
-          // console.log(response._embedded.events[i]._embedded.venues[j].name, response._embedded.events[i]._embedded.venues[j].city.name, response._embedded.events[i]._embedded.venues[j].state.name);
+        
           let venueName = response._embedded.events[i]._embedded.venues[j].name;
           let venueCity = response._embedded.events[i]._embedded.venues[j].city.name;
 
@@ -70,8 +54,6 @@ const getTM = function(artistName) {
             newDiv.html(`<p><strong>${venueName}</strong> at ${venueAddress} ${venueCity}, ${venueCountry}</p> <p><a href=${eventURL}>Link to Event</a></p>`);
 
             display.append(newDiv);
-
-            console.log("cashew");
           }
         }
       
@@ -82,14 +64,4 @@ const getTM = function(artistName) {
         }
       }
     });
-  // })
 };
-
-// _embedded.events[1]._embedded.venues[0].city
-// _embedded.events[0].url
-// _embedded.events[0]._embedded.venues
-// _embedded.events[3].dates.start.localDate
-// _embedded.venues[0].state
-// _embedded.venues[0].city
-// _embedded.venues[0].country
-// _embedded.events[1].classifications[0].segment.name
