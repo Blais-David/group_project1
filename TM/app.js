8a99982c488710621f8a21185537e8a961697c8c
Anna Nichols <43146489+anicho91@users.noreply.github.com>
2018-10-15 10:35:58 -0400

    Update app.js

--- TM/app.js
+++ TM/app.js
@@ -2,36 +2,16 @@
 const runArtistSearch = function(e) {
   e.preventDefault();
   const input = $("#artist").val().trim();
-//   const input2 = $("#userRadius").val();
-//   const input3 = $("#userCityName").val();
   $("#artist").val("");
-//   $("#userRadius").val("");
-//   $("#userCityName").val("");
   getTM(input);
-//   getImage(input);
 };
 
-<<<<<<< HEAD
-=======
-// // For location search
-// const runLocationSearch = function(e) {
-//   e.preventDefault();
-//   const input2 = $("#userRadius").val();
-//   const input3 = $("#userCityName").val();
-//   $("userRadius","#userCityName").val("");
-//   getTMloc(input2, input3);
-//   console.log(input2, input3)
-// };
-
-
->>>>>>> 6c60d78bc78258a58fe1b66fb4ad3225ac5b2e76
 //For Clearing data
 const clearDiv = function(e) {
   e.preventDefault();
   $("#display").empty();
   $("#display2").empty();
   $("#htmlAName").empty();
-  $("#artImage").empty();
   console.log("peanut");
 };
 
