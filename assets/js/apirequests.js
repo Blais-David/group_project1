56b8ae826b2d39ac135f6a93ee256a7c053e72e6
RachidaSK <rachida.koco@gmail.com>
2018-10-17 14:49:16 -0400

    almost scared Rachida

--- assets/js/apirequests.js
+++ assets/js/apirequests.js
@@ -1,40 +1,30 @@
 // Last.FM ajax call -- designed to retrieve a biography of the artist as well as an image
 const requestBio = function (userQuery) {
     const api_key = `2d3b9c4428453cbdd901c3f5715de047`;
-    const correctionURL = `https://ws.audioscrobbler.com/2.0/?method=artist.getCorrection&artist=${userQuery}&api_key=${api_key}&format=json`;
-    
+    const targetURL = `https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${userQuery}&autocorrect=1&api_key=${api_key}&format=json`;
 
     $.ajax({
         //url: `https://cors-anywhere.herokuapp.com/${targetURL}`,
-        url: correctionURL,
+        url: targetURL,
         method: 'GET'
-    }).then(function (call) {
-        const correctName = call.corrections.correction.artist.name
-        
-        const targetURL = `https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${correctName}&api_key=${api_key}&format=json`;
-        
-
-        $.ajax({
-            url:targetURL,
-            method: 'GET'
-        }).then(function(response) {
+    }).then(function (response) {
+         
         let bioParse = response.artist.bio.content;
-        console.log('bioparse:');
-        console.log(bioParse);
-        bioParse = bioParse.split(`\n`);
-        let bioFinal = `${bioParse[0]}\n${bioParse[2]}`; 
-
+        let bioFinal=  bioParse.slice(0, 1000);
+        
         // Get Image URL for user's query
         // response.artist.image[] produces an array of varying sizes of images
         // index of 2 = large image
         let imageURL = '';
         
         for (let i = 0; i < response.artist.image.length; i++) {
-            imageURL = response.artist.image[2]["#text"]; 
+            imageURL = response.artist.image[5]["#text"]; 
         }
+        // Create a link for users to read the rest of the bio
+        let bioUrl = response.artist.bio.links.link.href;
         // create final HTML components
-        const artistImage = $(`<img src="${imageURL}" class="card-img-left rounded mx-auto d-block" alt="artist picture">`);
-        const bio = $(`<p class="card-text text-justify mt-4">${bioFinal}</p>`);
+        const artistImage = $(`<img src="${imageURL}" class="card-img-left rounded d-block float-left mt-3 mr-4 mb-2" alt="artist picture">`);
+        const bio = $(`<p class="card-text text-justify mt-3">${bioFinal}<a href=${bioUrl}>...Read more</a></p><i><small><p style="color:gray; float:right; margin-bottom:10px">-Last.fm™</i></small></p>`);
 
         //clear out the artist-meta card div
         $('#artist-meta').html("");
@@ -44,8 +34,6 @@ const requestBio = function (userQuery) {
         $('#artist-meta').append(bio);
 
         //set the title of the card to the artists name
-        $('#artist-name').text(correctName);
-        });
-
+        $('#artist-name').text(response.artist.name);
     });
 };
